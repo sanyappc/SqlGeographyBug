eb10c5c54f04120187a525539d186e480baaed65
Александр Мокров <amokrov@zoho.com>
2018-04-18 21:42:50 +0300

    Bug

---
+++ Geography/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Data.SqlTypes;
+using System.Diagnostics;
+using Microsoft.SqlServer.Types;
+
+namespace Geography
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SqlServerTypes.Utilities.LoadNativeAssemblies(AppDomain.CurrentDomain.BaseDirectory);
+
+            SqlGeography sqlGeography = SqlGeography.STGeomFromText(new SqlChars(Wkt.WKT), 4326);
+            sqlGeography.MakeValid();
+
+            Debugger.Break();
+        }
+    }
+}
